402d7187bd7cff923469a4613dbdd96f3a16ac52
Filtox <pierre-du-12@hotmail.fr>
2020-01-14 22:54:03 +0100

    phpcv

--- Php/laragon/www/CV/cv.php
+++ Php/laragon/www/CV/cv.php
@@ -1,28 +1,28 @@
 <?php
 include_once 'cv_data.php';
+echo '<br> <br> <br>';
+echo '<h1>Présentation</h1>';
+echo '<br> <br> <br>';
 echo 'Bonjour, je suis ';
 foreach($informations as $element)
 {
     echo ' ' . $element;
 }
 echo '<br> <br> <br>';
+echo '<h1 style=>Scolarité</h1>';
 foreach($scolarite as $element) {
-    echo '<hr>';
+    echo '<br>' . '<hr>' . '<br>';
     foreach($element as $element1) {
         echo $element1 . '<br>';
     }
 }
 echo '<br> <br> <br>';
+echo '<h1>Expériences professionnelles</h1>';
 foreach($exp as $element) {
-    echo '<hr>';
+    echo '<br>' . '<hr>';
     foreach($element as $element1) {
         echo $element1 . '<br>';
     }
 }
+echo '<br> <br> <br>';
 ?>
-<style>
-    body {
-        background-color : black;
-        color : white;
-    }
-</style>
--- Php/laragon/www/CV/cv_data.php
+++ Php/laragon/www/CV/cv_data.php
@@ -33,38 +33,38 @@ $scolarite = [
 $exp = [
     [
         'annee' => '2019',
-        'entreprise' => 'BSPP',
-        'poste' => 'pompier'
+        'entreprise' => 'Brigade des Sapeurs Pompiers de Paris',
+        'poste' => 'Pompier'
     ],
     [
         'annee' => '2019',
         'entreprise' => 'French Col',
-        'poste' => 'trésorier adjoint'
+        'poste' => 'Trésorier adjoint'
     ],
     [
         'annee' => '2018',
         'entreprise' => 'Belet Isolations',
-        'poste' => 'Poseur d isolation'
+        'poste' => 'Poseur d\'isolation'
     ],
     [
         'annee' => '2018',
         'entreprise' => 'Protection civile',
-        'poste' => 'secouriste'
+        'poste' => 'Secouriste'
     ],
     [
         'annee' => '2017',
         'entreprise' => 'Brico Depot',
-        'poste' => 'vendeur'
+        'poste' => 'Vendeur'
     ],
     [
         'annee' => '2017',
         'entreprise' => 'Bosch',
-        'poste' => 'dessinateur 3D'
+        'poste' => 'Dessinateur 3D'
     ],
     [
         'annee' => '2016',
         'entreprise' => 'Leclerc',
-        'poste' => 'vendeur et mise en rayon'
+        'poste' => 'Vendeur et mise en rayon'
     ],
     [
         'annee' => '2015',
@@ -86,3 +86,17 @@ $exp = [
     $loisirs = ['airsoft', 'course a pied'];
     
 ?>
+<style>
+    body {
+        background-color : black;
+        color : white;
+        margin-left : 30px;
+    }
+    h1 {
+        color : red;
+        font-weight : bold;
+        margin-left : 100px;
+        text-decoration : underline;
+    }
+</style>
+<title>CV</title>
