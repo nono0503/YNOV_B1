dd1a30f4b2a4aae1b5f971df7d5bfc13cd145968
pierre <pierre.dasilva@ynov.com>
2020-01-15 15:22:13 +0100

    Create ActivatingGameObject.cs

---
+++ Certification/Script Beginner/ActivatingGameObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActiveObjects : MonoBehaviour
+{
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+}
+
+public class CheckState : MonoBehaviour
+{
+    public GameObject myObject;
+
+
+    void Start()
+    {
+        Debug.Log("Active Self: " + myObject.activeSelf);
+        Debug.Log("Active in Hierarchy" + myObject.activeInHierarchy);
+    }
+}
