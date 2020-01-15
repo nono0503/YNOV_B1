4a98777cc9b521e941c088a2df9a96775037e134
pierre <pierre.dasilva@ynov.com>
2020-01-15 12:38:00 +0100

    tutounity

---
+++ Certification/Script Beginner/AwakeAndStart.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AwakeAndStart : MonoBehaviour
+{
+    void Awake()    // Appelé en premier
+    {
+        Debug.Log("Awake called.");
+    }
+
+
+    void Start()    // Initialisé après Awake
+    {
+        Debug.Log("Start called.");
+    }
+}
