8c3c543eeb5e8db4c888f2c38be6b4c841d357c4
pierre <pierre.dasilva@ynov.com>
2020-01-15 10:59:54 +0100

    certif

---
+++ Certification/Script Beginner/VariablesAndFunctions.cs
@@ -0,0 +1,25 @@
+﻿// Ce script permet de multiplier un nombre par un autre et l'afficher dans la console de Unity
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class VariablesAndFunctions : MonoBehaviour
+{
+    int myInt = 5;
+
+
+    void Start()
+    {
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log(myInt);
+    }
+
+
+    int MultiplyByTwo(int number)
+    {
+        int ret;
+        ret = number * 2;
+        return ret;
+    }
+}
---
+++ Certification/Script Beginner/changeColor.cs
@@ -0,0 +1,24 @@
+﻿// Ce script permet de changer la couleur d'un objet dans le jeu. Il change le couleur à partir du composant Renderer.
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class changeColor : MonoBehaviour    // le nom du script dans le jeu se nomme changeColor
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+    }
+}
